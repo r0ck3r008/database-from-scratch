bf871ef6c6f849b70a5afed7c725316b66be9eaa
r0ck3r008 <naman.arora@tutanota.com>
2020-01-24 15:25:16 -0500

    implemented create, open and close basic templates, const char for fname still a problem

--- src/fs/dbfile.cc
+++ src/fs/dbfile.cc
@@ -1,32 +1,34 @@
 #include "dbfile.h"
-#include "glbl/defs.h"
 
-// stub file .. replace it with your own DBFile.cc
-
-DBFile::DBFile () {
-
+DBFile::DBFile()
+{
+	this->file=NULL;
 }
 
-int DBFile::Create (const char *f_path, fType f_type, void *startup) {
+DBFile::~DBFile()
+{
 }
 
-void DBFile::Load (Schema &f_schema, const char *loadpath) {
+int DBFile::Create(char *fname, fType file_type, void *startup)
+{
+	if(this->file==NULL){
+		file=new File;
+		this->file->Open(0, fname);
+	}else{
+		if(!this->Open(fname))
+			return 0;
+	}
+
+	return 1;
 }
 
-int DBFile::Open (const char *f_path) {
+int DBFile::Open(char *fname)
+{
+	this->file->Open(1, fname);
+	return 0;
 }
 
-void DBFile::MoveFirst () {
-}
-
-int DBFile::Close () {
-}
-
-void DBFile::Add (Record &rec) {
-}
-
-int DBFile::GetNext (Record &fetchme) {
-}
-
-int DBFile::GetNext (Record &fetchme, CNF &cnf, Record &literal) {
+int DBFile::Close()
+{
+	return this->file->Close();
 }
--- src/fs/dbfile.h
+++ src/fs/dbfile.h
@@ -1,32 +1,26 @@
 #ifndef DBFILE_H
 #define DBFILE_H
 
-#include "mem/two_way_list.h"
-#include "db/schema.h"
-#include "file.h"
-#include "record.h"
-#include "lex/comparison.h"
-#include "lex/comparison_engine.h"
+#include"mem/two_way_list.cc"
+#include"file.h"
 
-typedef enum {heap, sorted, tree} fType;
-
-// stub DBFile header..replace it with your own DBFile.h
-
-class DBFile {
-
-public:
-	DBFile ();
-
-	int Create (const char *fpath, fType file_type, void *startup);
-	int Open (const char *fpath);
-	int Close ();
-
-	void Load (Schema &myschema, const char *loadpath);
-
-	void MoveFirst ();
-	void Add (Record &addme);
-	int GetNext (Record &fetchme);
-	int GetNext (Record &fetchme, CNF &cnf, Record &literal);
-
+enum fType
+{
+	heap, sorted, tree;
 };
+
+class DBFile
+{
+	Record *pointer;
+	TwoWAyList <Page> pgs;
+	File *file;
+public:
+	DBFile();
+	~DBFile();
+	int Create(char *, fType, void *);
+	int Open(char *);
+	int Close();
+};
+
+
 #endif
