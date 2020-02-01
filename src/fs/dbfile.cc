17cef351157372755360b0a4335c47589810fa2d
r0ck3r008 <naman.arora@tutanota.com>
2020-02-01 16:22:26 -0500

    reimplementation yet again

--- src/fs/dbfile.cc
+++ src/fs/dbfile.cc
@@ -7,10 +7,10 @@
 DBFile :: DBFile()
 {
 	this->file=new File;
-	this->pg=new Page;
-	this->curr_pg=0;
 	this->head=NULL;
 	this->dirty=0;
+	this->curr_pg=0;
+	this->pg=new Page;
 }
 
 DBFile :: ~DBFile()
@@ -19,6 +19,7 @@ DBFile :: ~DBFile()
 	delete this->file;
 }
 
+//private functions
 void DBFile :: set_dirty()
 {
 	this->dirty=1;
@@ -36,10 +37,8 @@ int DBFile :: chk_dirty()
 
 void DBFile :: writeback()
 {
-	this->file->AddPage(this->pg, curr_pg++);
+	this->file->AddPage(this->pg, this->curr_pg++);
 	this->pg->EmptyItOut();
-	delete this->pg;
-	this->pg=new Page;
 	this->unset_dirty();
 }
 
@@ -47,10 +46,12 @@ void DBFile :: fetch(off_t pg_num)
 {
 	if(this->chk_dirty())
 		this->writeback();
+
 	this->file->GetPage(this->pg, pg_num);
-	this->curr_pg=pg_num;
 }
 
+
+//public functions
 int DBFile :: Create(const char *fname, fType type, void *startup)
 {
 	this->file->Open(0, fname);
@@ -59,6 +60,8 @@ int DBFile :: Create(const char *fname, fType type, void *startup)
 
 int DBFile :: Open(const char *fname)
 {
+	//TODO
+	//dummy proofing, use stat to acertain that the file exists
 	this->file->Open(1, fname);
 	this->fetch(0);
 	return 1;
@@ -66,73 +69,73 @@ int DBFile :: Open(const char *fname)
 
 void DBFile :: MoveFirst()
 {
-	if(this->curr_pg)
-		this->fetch(0);
 
-	this->pg->myRecs->MoveToStart();
-	this->head=this->pg->myRecs->Current(0);
-}
-
-void DBFile :: Add(Record *new_rec)
-{
-	if(this->pg->get_curr_size() + new_rec->get_size() > PAGE_SIZE)
-		//writeback to disk
-		this->writeback();
-
-	if(!this->pg->Append(new_rec)) {
-		std :: cerr << "Error in adding new rec!" << std :: endl;
-		_exit(-1);
-	}
-	this->set_dirty();
 }
 
 int DBFile :: GetNext(Record **placeholder)
 {
-	if(!this->pg->myRecs->RightLength())
-		this->fetch(++this->curr_pg);
-
-	this->head=this->pg->myRecs->Current(0);
-	*placeholder=this->head;;
-	this->pg->myRecs->Advance();
 	return 1;
 }
 
-void DBFile :: Load(Schema *sch, char *fname)
+void DBFile :: Add(Record *placeholder)
+{
+	off_t curr_len=this->file->GetLength();
+
+	if(curr_len==0 || this->curr_pg==curr_len-1) {
+		//this is on latest page
+		//check size and writeback if necessary
+		if(this->pg->get_curr_size() + placeholder->get_size() >
+			PAGE_SIZE)
+			this->writeback();
+	} else {
+		//fetch latest page for reading... this is a read write
+		//alternating situation
+		this->fetch(curr_len-1);
+	}
+
+	if(!this->pg->Append(placeholder)) {
+		std :: cerr << "Error in adding record!\n";
+		_exit(-1);
+	}
+
+	if(!chk_dirty())
+		this->set_dirty();
+}
+
+void DBFile :: Load(Schema *sch, const char *fname)
 {
 	FILE *f=NULL;
 	if((f=fopen(fname, "r"))==NULL) {
-		std :: cerr << "Error in opening file at " << fname
+		std :: cerr << "Error in opening the file at " << fname
 			<< std :: endl;
 		_exit(-1);
 	}
 
-	//NOTE
-	//feof returns 1 if it is the end of file else 0
-	Record *tmp=NULL;
+	Record *tmp=new Record;
 	while(1) {
-		int stat=0;
-		tmp=new Record;
-		stat=tmp->SuckNextRecord(sch, f);
+		int stat=tmp->SuckNextRecord(sch, f);
 		if(!stat) {
 			if(feof)
-				std :: cout << "EOF!" << std :: endl;
+				std :: cout << "EOF!\n";
 			else
-				std :: cout << "Error in getting new record "
-					<< std :: endl;
+				std :: cout << "Error!\n";
 			break;
 		}
+
 		this->head=tmp;
 		this->Add(tmp);
+
+		tmp=new Record;
 	}
 
-	std :: cout << "Exiting!\n";
 	fclose(f);
 }
 
 int DBFile :: Close()
 {
-	this->writeback();
-	if(!this->file->Close())
-		return 0;
+	if(this->chk_dirty())
+		this->writeback();
+
+	this->file->Close();
 	return 1;
 }
