36dcedf66a1774dfa1548a6d01a2a0e43af7a2dd
r0ck3r008 <naman.arora@tutanota.com>
2020-02-14 10:32:37 -0500

    added constructors and destructors

--- src/mem/run_gen.cc
+++ src/mem/run_gen.cc
@@ -1,13 +1,16 @@
 #include"run_gen.h"
 
-RunGen :: RunGen(Pipe *in_pipe, int run_len)
+RunGen :: RunGen(Pipe *in_pipe, int run_len, OrderMaker *order)
 {
 	this->in_pipe=in_pipe;
 	this->run_len=run_len;
-	this->tour=new Tournament <Record>(run_len/4);
+	this->order=order;
+	this->comp=new struct comparator(NULL, NULL, (void *)order, NULL, 0);
+	this->tour=new Tournament <Record> (run_len/4, comp);
 }
 
 RunGen :: ~RunGen()
 {
+	delete this->comp;
 	delete this->tour;
 }
--- src/mem/run_gen.h
+++ src/mem/run_gen.h
@@ -1,6 +1,7 @@
 #ifndef RUN_GEN_H
 #define RUN_GEN_H
 
+#include"lex/comparison_engine.h"
 #include"tournament.cc"
 #include"fs/record.h"
 #include"pipe.h"
@@ -11,9 +12,12 @@ private:
 	Pipe *in_pipe;
 	Tournament <Record> *tour;
 	int run_len;
+	struct comparator *comp;
+	OrderMaker *order;
+
 
 public:
-	RunGen(Pipe *, int);
+	RunGen(Pipe *, int, OrderMaker *);
 	~RunGen();
 };
 
