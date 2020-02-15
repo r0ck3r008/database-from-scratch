df58234667c3ef17f57e225591e1b0af89b7420f
r0ck3r008 <naman.arora@tutanota.com>
2020-02-15 12:57:27 -0500

    added correct addresses

--- src/test.h
+++ src/test.h
@@ -11,9 +11,9 @@ using namespace std;
 
 // make sure that the information below is correct
 
-char *catalog_path = "catalog";
-char *tpch_dir ="/cise/tmp/dbi_sp11/DATA/10M/"; // dir where dbgen tpch files (extension *.tbl) can be found
-char *dbfile_dir = "";
+char *catalog_path = "db/catalog";
+char *tpch_dir ="tpch-dbgen/"; // dir where dbgen tpch files (extension *.tbl) can be found
+char *dbfile_dir = "out/";
 
 
 extern "C" {
