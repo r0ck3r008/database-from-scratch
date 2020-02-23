c5ee70d19b45f00415a99aa4364f65d5e4204f11
r0ck3r008 <naman.arora@tutanota.com>
2020-02-23 17:33:56 -0500

    Implemented SortedFile functions set_dirty, fetch, Open, Add, Load and MoveFirst

--- src/fs/sorted.cc
+++ src/fs/sorted.cc
@@ -1,4 +1,35 @@
+#include<unistd.h>
+
 #include"sorted.h"
+
+void SortedFile :: set_dirty()
+{
+	if(this->in_pipe==NULL)
+		this->in_pipe=new Pipe(100);
+	if(this->out_pipe==NULL)
+		this->out_pipe=new Pipe(100);
+	if(this->bigq==NULL)
+		this->bigq=new BigQ(this->in_pipe, this->out_pipe,
+					this->s_info->order,
+					this->s_info->run_len);
+	this->dirty=1;
+}
+
+void SortedFile :: unset_dirty()
+{
+	this->dirty=0;
+}
+
+int SortedFile :: chk_dirty()
+{
+	return (this->dirty) ? 1 : 0;
+}
+
+void SortedFile :: fetch(int pg_num)
+{
+	this->file->GetPage(this->pg, pg_num);
+	this->curr_pg=pg_num;
+}
 
 SortedFile :: SortedFile(File *file, Page *pg, struct SortInfo *s_info)
 {
@@ -13,21 +44,7 @@ SortedFile :: SortedFile(File *file, Page *pg, struct SortInfo *s_info)
 	this->curr_pg=0;
 }
 
-void SortedFile :: set_dirty()
-{
-	this->dirty=1;
-}
-
-void SortedFile :: unset_dirty()
-{
-	this->dirty=0;
-}
-
-int SortedFile :: chk_dirty()
-{
-	return (this->dirty) ? 1 : 0;
-}
-
+//public
 SortedFile :: ~SortedFile()
 {
 	if(this->in_pipe!=NULL)
@@ -41,35 +58,72 @@ SortedFile :: ~SortedFile()
 int SortedFile :: Create(const char *fname)
 {
 	int ret=1;
-
+	if(!this->file->Open(0, fname))
+		ret=0;
 	return ret;
 }
 
 int SortedFile :: Open(const char *fname)
 {
 	int ret=1;
-
+	if(!this->file->Open(1, fname))
+		ret=0;
+	this->fetch(0);
 	return ret;
 }
 
 void SortedFile :: Add(Record *in)
 {
+	if(!this->chk_dirty())
+		this->set_dirty();
 
+	this->in_pipe->Insert(in);
 }
 
 void SortedFile :: Load(Schema *sch, const char *fname)
 {
+	FILE *f=NULL;
+	if((f=fopen(fname, "r"))==NULL) {
+		std :: cerr << "Error in opening the file "
+			<< fname << "!\n";
+		_exit(-1);
+	}
 
+	Record *tmp=new Record;
+	while(1) {
+		int stat=tmp->SuckNextRecord(sch, f);
+		if(!stat) {
+			if(!feof)
+				std :: cerr << "Error!\n";
+			else
+				std :: cerr << "EOF!\n";
+			break;
+		}
+
+		this->head=tmp;
+		this->Add(tmp);
+		delete tmp;
+		tmp=new Record;
+	}
+
+	delete tmp;
+	fclose(f);
 }
 
 void SortedFile :: MoveFirst()
 {
+	if(this->chk_dirty())
+		this->unset_dirty();
 
+	if(!this->curr_pg)
+		this->fetch(0);
 }
 
 int SortedFile :: GetNext(Record *placeholder)
 {
 	int ret=1;
+	if(this->chk_dirty())
+		this->MoveFirst();
 
 	return ret;
 }
