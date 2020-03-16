100dd0ba0683027945b343fbb4513f138a1d25a6
r0ck3r008 <naman.arora@tutanota.com>
2020-03-16 03:51:38 -0400

    downgraded heap_gen version the original submission 1 test driver

--- src/tests/heap_gen.cc
+++ src/tests/heap_gen.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-#include<unistd.h>
 
 #include"glbl/timer.h"
 #include "test.h"
@@ -9,7 +8,7 @@ using namespace std;
 relation *rela;
 
 // load from a tpch file
-void test () {
+void test1 () {
 
 	DBFile dbfile;
 	cout << " DBFile will be created at " << rela->path () << endl;
@@ -23,60 +22,99 @@ void test () {
 	dbfile.Close ();
 }
 
-void display()
-{
-	DBFile *dbf=new DBFile;
-	if(!dbf->Open(rela->path())){
-		cerr << "Cannot Open!\n";
-		_exit(-1);
-	}
+// sequential scan of a DBfile
+void test2 () {
 
-	dbf->MoveFirst();
-	Record *tmp=new Record;
-	int count=0;
-	while(1) {
-		if(!dbf->GetNext(tmp))
-			break;
-		tmp->Print(rela->schema());
-		delete tmp;
-		tmp=new Record;
-		count++;
-	}
+	DBFile dbfile;
+	dbfile.Open (rela->path());
+	dbfile.MoveFirst ();
 
-	cout << "Count= " << count << endl;
-	delete tmp;
+	Record *temp=new Record;
+
+	int counter = 0;
+	while (dbfile.GetNext (temp) == 1) {
+		counter += 1;
+		temp->Print (rela->schema());
+		if (counter % 10000 == 0) {
+			cout << counter << "\n";
+		}
+		delete temp;
+		temp=new Record;
+	}
+	delete temp;
+	cout << " scanned " << counter << " recs \n";
+	dbfile.Close ();
+}
+
+// scan of a DBfile and apply a filter predicate
+void test3 () {
+
+	cout << " Filter with CNF for : " << rela->name() << "\n";
+
+	CNF cnf;
+	Record literal;
+	rela->get_cnf (cnf, literal);
+	timer t;
+
+	DBFile dbfile;
+	dbfile.Open (rela->path());
+	dbfile.MoveFirst ();
+
+	Record *temp=new Record;
+
+	t.start_timer();
+	int counter = 0;
+	while (dbfile.GetNext (temp, &cnf, &literal) == 1) {
+		counter += 1;
+		temp->Print (rela->schema());
+		if (counter % 10000 == 0) {
+			cout << counter << "\n";
+		}
+		delete temp;
+		temp=new Record;
+	}
+	t.stop_timer();
+	struct timeval diff=t.get_tt();
+	delete temp;
+	cout << " selected " << counter << " recs \n";
+	cout << "Time taken: " << diff.tv_usec/1000.00 << " milliseconds!\n";
+	dbfile.Close ();
 }
 
 int main () {
 
 	setup ();
-	void (*test_ptr[])()={test, display};
-	void (*test)();
 
-	int tindx=0;
-	while(tindx<1 || tindx>2) {
-		cout << "\n Select test: \n";
-		cout << "\t 1. Make Heap File \n";
-		cout << "\t 2. Display HEap File \n\t";
+	void (*test) ();
+	void (*test_ptr[]) () = {&test1, &test2, &test3};
+
+	int tindx = 0;
+	while (tindx < 1 || tindx > 3) {
+		cout << " select test: \n";
+		cout << " \t 1. load file \n";
+		cout << " \t 2. scan \n";
+		cout << " \t 3. scan & filter \n \t ";
 		cin >> tindx;
 	}
 
 	int findx = 0;
 	while (findx < 1 || findx > 8) {
 		cout << "\n select table: \n";
-		cout << "\t 1. nation \n";
-		cout << "\t 2. region \n";
-		cout << "\t 3. customer \n";
-		cout << "\t 4. part \n";
-		cout << "\t 5. partsupp \n";
-		cout << "\t 6. supplier \n";
-		cout << "\t 7. orders \n";
+		cout << "\t 1. supplier \n";
+		cout << "\t 2. partsupp \n";
+		cout << "\t 3. part \n";
+		cout << "\t 4. nation \n";
+		cout << "\t 5. customer \n";
+		cout << "\t 6. orders \n";
+		cout << "\t 7. region \n";
 		cout << "\t 8. lineitem \n \t ";
 		cin >> findx;
 	}
 
 	rela = rel[findx-1];
-	test=test_ptr[tindx-1];
-	test();
+	test = test_ptr [tindx - 1];
+
+	test ();
+
 	cleanup ();
 }
