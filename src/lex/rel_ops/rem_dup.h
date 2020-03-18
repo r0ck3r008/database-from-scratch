343d4de7866248e25ae517821aea1d71d2e5457a
r0ck3r008 <naman.arora@tutanota.com>
2020-03-18 03:19:11 -0400

    Duplicate Removal Complete

---
+++ src/lex/rel_ops/rem_dup.h
@@ -0,0 +1,31 @@
+#ifndef REM_DUP_H
+#define REM_DUP_H
+
+#include"../rel_op.h"
+
+struct dup_args
+{
+public:
+	Pipe *in_pipe;
+	Pipe *out_pipe;
+	OrderMaker *order;
+};
+
+class DuplicateRemoval
+{
+private:
+	struct dup_args *args;
+	int max_pgs;
+	pthread_t tid;
+
+public:
+	DuplicateRemoval();
+	~DuplicateRemoval();
+	void Run(Pipe *, Pipe *, Schema *);
+	void WaitUntilDone();
+	void Use_n_Pages(int);
+};
+
+void *dup_thr(void *);
+
+#endif
