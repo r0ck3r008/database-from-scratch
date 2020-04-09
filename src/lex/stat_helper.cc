aba60481d4dc5bb9348800b3e2677b5f6fa3fa32
r0ck3r008 <naman.arora@tutanota.com>
2020-04-08 22:53:19 -0400

    added f_handle

---
+++ src/lex/stat_helper.cc
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<stdio.h>
+#include<string.h>
+#include<sys/stat.h>
+#include<sys/types.h>
+#include<unistd.h>
+
+#include"statistics.h"
+
+FILE *Statistics :: f_handle(char *fname, const char *perm)
+{
+	FILE *f=NULL;
+	int flag=1;
+	struct stat buf;
+	int res=stat(fname, &buf);
+	if(res==0 && !strcmp(perm, "w")) {
+		std :: cerr << "Overwriting file " << fname
+			<< std :: endl;
+	} else if(res<0 && !strcmp(perm, "r")) {
+		std :: cerr << "File " << fname << " doesnt exist!\n";
+		f=this->f_handle(fname, std :: string("w").c_str());
+		flag=0;
+	}
+
+	if(flag) {
+		if((f=fopen(fname, perm))==NULL) {
+			std :: cerr << "Error in opening file " << fname
+				<< " in " << perm << " mode!\n";
+			return NULL;
+		}
+	}
+
+	return f;
+}
