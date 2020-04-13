610f23e884a1e1a4c6c1d66581679d6909b2daa1
r0ck3r008 <naman.arora@tutanota.com>
2020-04-13 01:30:26 -0400

    redesigned statistics without attInfo dependence

--- src/lex/statistics.cc
+++ src/lex/statistics.cc
@@ -43,22 +43,14 @@ void Statistics :: AddAtt(char *_r_name, char *_a_name, int n_distinct)
 {
 	string r_name(_r_name);
 	string a_name(_a_name);
-	auto itr=this->attMap.find(a_name);
-	if(itr!=this->attMap.end() && !itr->second.relName.compare(r_name)) {
-		cerr << "Attribute under same relation already exists!\n";
-		return;
-	}
 	auto itr1=this->relMap.find(r_name);
 	if(itr1==this->relMap.end()) {
 		cerr << "Such a relation doesnt exit!\n";
 		return;
 	}
 
-	attInfo a_info;
-	a_info.n_distinct=(n_distinct==-1) ? (itr1->second.numTuples) :
-								(n_distinct);
-	a_info.relName=r_name;
-	this->attMap.insert(pair<string, attInfo>(a_name, a_info));
+	int n_dist=(n_distinct==-1) ? (itr1->second.numTuples) : (n_distinct);
+	itr1->second.attMap.insert(pair<string, int>(a_name, n_dist));
 }
 
 void Statistics :: CopyRel(char *_o_name, char *_n_name)
@@ -72,15 +64,6 @@ void Statistics :: CopyRel(char *_o_name, char *_n_name)
 	}
 
 	this->relMap.insert(pair<string, relInfo>(n_name, itr->second));
-	vector<attInfo> pending;
-	for(auto &itr: this->attMap) {
-		if(itr.second.relName.compare(o_name)==0) {
-			attInfo a_info=itr.second;
-			a_info.relName=n_name;
-			this->attMap.insert(pair<string, attInfo>(n_name,
-								a_info));
-		}
-	}
 }
 
 void Statistics :: Read(char *fname)
@@ -88,7 +71,10 @@ void Statistics :: Read(char *fname)
 	//String to get rid of those pesky warnings
 	FILE *f=this->f_handle(fname, string("r").c_str());
 	char *line=NULL;
+	string rname_curr;
+	relInfo curr_rel;
 	size_t n=0;
+	int begin=1;
 	while(!feof) {
 		ssize_t stat=getline(&line, &n, f);
 		if(stat==-1)
@@ -96,18 +82,21 @@ void Statistics :: Read(char *fname)
 
 		char *type=strtok(line, ":");
 		if(!strcmp(type, "R_BEGIN")) {
-			relInfo r_info;
-			char *r_name=strtok(NULL, ":");
-			r_info.numTuples=strtol(strtok(NULL, ":"), NULL, 10);
-			r_info.relCount=strtol(strtok(NULL, ":"), NULL, 10);
-			this->relMap.insert(pair<string, relInfo>
-						(string(r_name), r_info));
-
+			if(!begin) {
+				this->relMap.insert(pair<string, relInfo>
+							(rname_curr, curr_rel));
+				explicit_bzero(&curr_rel, sizeof(relInfo));
+			} else {
+				begin=0;
+			}
+			rname_curr=string(strtok(NULL, ":"));
+			curr_rel.numTuples=strtol(strtok(NULL, ":"), NULL, 10);
+			curr_rel.relCount=strtol(strtok(NULL, ":"), NULL, 10);
 		} else if(!strcmp(type, "A_BEGIN")) {
-			char *a_name=strtok(NULL, ":");
-			char *r_name=strtok(NULL, ":");
-			int num_distinct=strtol(strtok(NULL, ":"), NULL, 10);
-			this->AddAtt(r_name, a_name, num_distinct);
+			string a_name(strtok(NULL, ":"));
+			int distinct=strtol(strtok(NULL, ":"), NULL, 10);
+			curr_rel.attMap.insert(pair<string, int>
+							(a_name, distinct));
 		}
 
 		free(line);
@@ -122,13 +111,14 @@ void Statistics :: Write(char *fname)
 {
 	FILE *f=this->f_handle(fname, string("w").c_str());
 
-	for(auto &itr: this->relMap)
-		fprintf(f, "R_BEGIN:%s:%d:%d\n", itr.first.c_str(),
-			itr.second.numTuples, itr.second.relCount);
-
-	for(auto &itr: this->attMap)
-		fprintf(f, "A_BEGIN:%s:%s:%d\n", itr.first.c_str(),
-			itr.second.relName.c_str(), itr.second.n_distinct);
+	for(auto &i: this->relMap) {
+		fprintf(f, "R_BEGIN:%s:%d:%d\n", i.first.c_str(),
+			i.second.numTuples, i.second.relCount);
+		for(auto &itr: i.second.attMap) {
+			fprintf(f, "A_BEGIN:%s:%d\n", itr.first.c_str(),
+							itr.second);
+		}
+	}
 
 	fclose(f);
 }
