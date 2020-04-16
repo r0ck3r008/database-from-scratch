aa32f3d23236dc0469896e2a601a75920808a5c5
r0ck3r008 <naman.arora@tutanota.com>
2020-04-15 21:40:10 -0400

    created a sub group for scripts

---
+++ src/scripts/create_heap_files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+printf 1"\n"1 | ../heap_gen.out;
+printf 1"\n"2 | ../heap_gen.out;
+printf 1"\n"3 | ../heap_gen.out;
+printf 1"\n"4 | ../heap_gen.out;
+printf 1"\n"5 | ../heap_gen.out;
+printf 1"\n"6 | ../heap_gen.out;
+printf 1"\n"7 | ../heap_gen.out;
+printf 1"\n"8 | ../heap_gen.out;
