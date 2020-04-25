dee41d926375e95b0893428c16fe4573352826de
r0ck3r008 <naman.arora@tutanota.com>
2020-04-25 12:48:57 -0400

    Repurposed Schema to struct relInfo and struct Attribute to struct attInfo

--- src/db/schema.h
+++ src/db/schema.h
@@ -1,16 +1,30 @@
 #ifndef SCHEMA_H
 #define SCHEMA_H
 
+#include<unordered_map>
 
+#include"glbl/defs.h"
 
+struct Attribute
+{
+	int n_dis;
 	Type myType;
 };
 
+struct Schema
+{
+	std::string fname;
+	int n_tup;
+	fType type;
+	std::unordered_map<std::string, Attribute> attMap;
 
+	Schema();
+	~Schema ();
 	int Find (char *attName);
 	Type FindType (char *attName);
 	void Print();
 };
 
+void splice(char *, std::string *, std::string *);
 
 #endif
