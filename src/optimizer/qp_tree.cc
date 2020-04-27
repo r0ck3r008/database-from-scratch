564ece6fd0b6479aad3c26008146b976bbcc60b7
r0ck3r008 <naman.arora@tutanota.com>
2020-04-27 11:42:48 -0400

    completed final join interface

--- src/optimizer/qp_tree.cc
+++ src/optimizer/qp_tree.cc
@@ -1,3 +1,5 @@
+#include<unistd.h>
+
 #include"qp_tree.h"
 
 Qptree :: Qptree(Catalog *cat)
@@ -6,6 +8,7 @@ Qptree :: Qptree(Catalog *cat)
 	this->s=new Statistics(cat);
 	this->curr_pipe_id=0;
 	this->curr_pipe=NULL;
+	this->tree=NULL;
 }
 Qptree :: ~Qptree()
 {
@@ -16,6 +19,26 @@ void Qptree :: process(struct query *q)
 {
 	this->process(q->tbls);
 	this->mk_ops(q->a_list);
+
+	vector<operation *> vec;
+	while(this->join_queue.size()!=0) {
+		struct operation *op=this->join_queue.top();
+		this->join_queue.pop();
+		this->process(op, vec);
+	}
+	if(tree==NULL) {
+		//in case of no join
+		for(auto &i: relations) {
+			struct operation *op=NULL;
+			if((op=i.second->dispense_sel(this))==NULL) {
+				cerr << "Error in fetching the select!\n";
+				_exit(-1);
+			}
+			if(this->tree!=NULL)
+				mk_parent(this, op, this->tree, 0);
+			this->tree=op;
+		}
+	}
 }
 
 Pipe *Qptree :: dispense_pipe(int *pipe_id)
