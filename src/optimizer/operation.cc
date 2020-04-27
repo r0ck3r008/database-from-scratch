1fa7e1a384dd8fe92fc7458025add49455af4eca
r0ck3r008 <naman.arora@tutanota.com>
2020-04-27 11:41:18 -0400

    refer commit eb3f40f83

--- src/optimizer/operation.cc
+++ src/optimizer/operation.cc
@@ -1,10 +1,13 @@
+#include<iostream>
+
 #include"operation.h"
+
+using namespace std;
 
 operation :: operation(type_flag type, double cost, vector<tableInfo *> &vec)
 {
 	this->lid=-1; this->rid=-1; this->pid=-1;
 	this->lchild=NULL; this->rchild=NULL; this->parent=NULL;
-	this->lpipe=NULL; this->rpipe=NULL; this->ppipe=NULL;
 	this->cost=cost;
 	this->type=type;
 	this->tables=vector<tableInfo *>(vec);
