51a54479d84a0381d8bbe877ed057ba328767f2e
r0ck3r008 <naman.arora@tutanota.com>
2020-04-27 10:14:01 -0400

    refer commit 1dd90776

--- src/optimizer/operation.cc
+++ src/optimizer/operation.cc
@@ -4,6 +4,7 @@ operation :: operation(type_flag type, double cost, vector<tableInfo *> &vec)
 {
 	this->lid=-1; this->rid=-1; this->pid=-1;
 	this->lchild=NULL; this->rchild=NULL; this->parent=NULL;
+	this->lpipe=NULL; this->rpipe=NULL; this->ppipe=NULL;
 	this->cost=cost;
 	this->type=type;
 	this->tables=vector<tableInfo *>(vec);
